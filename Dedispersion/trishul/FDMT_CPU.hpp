d29d1bc3c22e95d7e4cf4b77011fe19c7d0eac20
shiningsurya <15906282+shiningsurya@users.noreply.github.com>
2019-12-17 20:46:31 -0600

    DL

---
+++ Dedispersion/trishul/FDMT_CPU.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include "trishul.hpp"
+#include "trishul/Dedisperser.hpp"
+
+#include <cmath>
+
+class FDMT_CPU : public TrishulDedisperser {
+	private:
+		// Main state	
+		Unsigned_t      nsamps;
+		FloatVector_t   State;
+		// DM ranges
+		Unsigned_t      aDT;
+		Unsigned_t      bDT;
+		Unsigned_t      maxDT;
+		// Freq
+		float_t         fmin;
+		float_t         fmax;
+		unsigned_t      l2nch;
+		//
+		void Initialization (const FloatVector_t& );
+		void Iteration (const unsigned_t& );
+		static inline float_t cff (float_t f0, float_t f1, float_t f2) {
+				return (std::pow(f0,-2) - std::pow(f1,-2))/(std::pow(f0,-2) - std::pow(f2,-2));
+		}
+		static inline float_t dff (float_t f0, float_t f1) {
+				return std::pow (f0,-2) - std::pow (f1,-2);
+		}
+	public:
+		FDMT_CPU ()  = default;
+		~FDMT_CPU () = default;
+
+		void CreatePlan (const double_t, const unsigned_t, const float_t, const float_t) override;
+
+		void Execute (const FloatVector_t&, Unsigned_t, FloatVector_t&) override;
+		void Execute (const ByteVector_t&,  Unsigned_t, ByteVector_t& ) override {
+			throw TrishulError ("Not available for FDMT_CPU.");
+		}
+};
---
+++ Learnings/Kurtosis.cpp
@@ -0,0 +1,10 @@
+#include "trishul.hpp"
+#include "trishul/Features.hpp"
+
+float_t tf::Kurtosis (const FloatVector_t& fv, Unsigned_t size) {
+
+}
+
+float_t tf::Skewness (const FloatVector_t& fv, Unsigned_t size) {
+
+}
